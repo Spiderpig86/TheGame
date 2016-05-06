7ce7b5276485da97c02bb83024aa5e4fba2463c7
Stan <slim679975@gmail.com>
2016-05-06 18:49:22 -0400

    Updated HUD, new enemies, and lots of other new stuff.

---
+++ src/GameMain/Spawn.java
@@ -0,0 +1,38 @@
+package GameMain;
+
+import java.util.Random;
+
+/**
+ * Created by Stan on 5/6/2016.
+ */
+public class Spawn { //Spawns enemies, keeps track of score.
+
+    private Handler handler;
+    private HUD hud;
+    Random r;
+
+    private int scoreKeep = 0;
+
+    public Spawn(Handler handler, HUD hud) {
+        this.handler = handler;
+        this.hud = hud;
+        r = new Random();
+    }
+
+    public void tick() {
+        scoreKeep++;
+
+        if (scoreKeep >= 1000) {
+            scoreKeep = 0;
+            hud.setLevel(hud.getLevel() + 1);
+
+            //Add T1 Square each level.
+
+            handler.addObject(new T1Square(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT), ID.T1Enemy, handler));
+
+            if(hud.getLevel() / 5 == 0 {
+                handler.addObject(new T3Square(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT), ID.T3Enemy, handler));
+            }
+        }
+    }
+}
