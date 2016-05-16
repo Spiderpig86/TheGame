19fce6cc137150ec890705f454fa85955d7e3e78
Spiderpig86 <slim679975@gmail.com>
2016-05-16 14:01:52 -0400

    Add files via upload
    Added health entity to restore HP.

---
+++ src/GameMain/Health.java
@@ -0,0 +1,36 @@
+package src.GameMain;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Health extends GameObject
+{
+    // instance variables - replace the example below with your own
+    private Handler handler;
+
+    /**
+     * Constructor for objects of class Health
+     */
+    public Health(int x, int y, ID id, Handler handler)
+    {
+        super(x, y, id);
+        
+        velX = 0;
+        velY = 0;
+        
+        this.handler = handler;
+    }
+    
+     public Rectangle getBounds() {
+        return new Rectangle((int)x, (int)y, 32, 32);
+    }
+
+    public void tick() {
+        //stationary does not do update
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.drawRect((int)x, (int)y, 16, 16);
+    }
+}
